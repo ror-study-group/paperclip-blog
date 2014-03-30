9280b41fd39dca4b34dcf125203d74d66745b2d5
Brooks Swinnerton <bswinnerton@gmail.com>
2014-03-30 13:10:30 -0400

    Adding paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ RorBlog::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # S3 credentials
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['ROR_BLOG_S3_BUCKET_NAME'],
+      :access_key_id => ENV['ROR_BLOG_AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['ROR_BLOG_AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
